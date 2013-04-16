2ea68647b39bd167f73708108ceef51ea99d54e3
Cas Ebbers <cjaebbers@gmail.com>
2013-04-16 14:55:54 +0200

    Deploy with capistrano

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 gem 'sqlite3'
+gem 'mysql'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
@@ -17,3 +18,5 @@ gem 'meta_search'
 gem 'simple_form'
 gem 'ancestry'
 gem 'will_paginate'
+
+gem 'capistrano'
---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'bundler/capistrano'
+
+set :application, "portal"
+set :domain,      "www.casebbers.net"
+set :repository,  "git://github.com/Eat-and-Drive/portal.git"
+set :use_sudo,    false
+set :deploy_to,   "/srv/railsapps/#{application}"
+set :scm,         "git"
+set :user,        "eatdrive"
+set :port,        "2287"
+set :scm_password, ""
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+  
+  desc "Symlink database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
+  end
+end
+
+after "deploy:update_code", "deploy:migrate"
+after "deploy:update_code", "deploy:symlink_db"
